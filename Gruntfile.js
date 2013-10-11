6cf250931e2c9bf62f7b75b11a1eb8dd89f2e8df
Franco Bouly <fbouly@gmail.com>
2013-10-11 17:34:09 +0200

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,13 @@
 'use strict';
 
 var LIVERELOAD_PORT = 35729,
-    lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+    lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT }),
+    path = {
+      build: {
+        dev: 'build/dev/',
+        prod: 'build/prod/'
+      }
+    };
 
 var mountFolder = function (connect, dir) {
   return connect.static(require('path').resolve(dir));
@@ -22,6 +28,7 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    path: path,
     pkg: grunt.file.readJSON('package.json'),
     vendor: grunt.file.readJSON('.bowerrc').directory,
     config: grunt.file.readYAML('src/data/config.yml'),
@@ -32,6 +39,7 @@ module.exports = function(grunt) {
         flatten: true,
         data: 'src/data/*.{json,yml}',
         ext: '.html',
+        engine: 'handlebars',
         helpers: ['src/templates/helpers/helper-*.js'],
         partials: ['src/templates/partials/**/*.hbs'],
         layoutdir: 'src/templates/layouts',
@@ -42,25 +50,26 @@ module.exports = function(grunt) {
         options: {
           layout: 'page.hbs'
         },
-        files: {'www/': ['src/content/*.md']}
+        files: {'<%= path.build.dev %>': ['src/content/*.md']}
       },
       fr: {
         options: {
           layout: 'page.hbs'
         },
-        files: {'www/fr/': ['src/content/fr/*.md']}
+        files: {'<%= path.build.dev %>/fr/': ['src/content/fr/*.md']}
       },
       en: {
         options: {
           layout: 'page.hbs'
         },
-        files: {'www/en/': ['src/content/en/*.md']}
+        files: {'<%= path.build.dev %>/en/': ['src/content/en/*.md']}
       }
     },
 
     // Remove previously created files
     clean: {
-      dest: ['www']
+      dev: ['<%= path.build.dev %>'],
+      prod: ['<%= path.build.prod %>']
     },
 
     // Create a Server
@@ -74,7 +83,7 @@ module.exports = function(grunt) {
           middleware: function (connect) {
             return [
               lrSnippet,
-              mountFolder(connect, 'www')
+              mountFolder(connect, '<%= path.build.dev %>')
             ];
           }
         }
@@ -92,7 +101,7 @@ module.exports = function(grunt) {
     copy: {
       assets: {
         files: [
-          {expand: true, cwd: 'public/', src: ['**'], dest: 'www/'}, // includes files in path
+          {expand: true, cwd: 'public/', src: ['**'], dest: '<%= path.build.dev %>'}, // includes files in path
         ]
       }
     },
@@ -116,7 +125,7 @@ module.exports = function(grunt) {
         tasks: ['copy']
       },
       livereload: {
-        files: ['www/**/*'],
+        files: ['<%= path.build.dev %>**/*'],
         options: {
           livereload: true
         }
@@ -158,13 +167,6 @@ module.exports = function(grunt) {
         engine: 'phantomjs',
         padding: 2
       }
-      all2x: {
-        src: ['src/sprites@2x/*.png'],
-        destImg: 'public/img/spritesheet@2x.png',
-        destCSS: 'src/sass/includes/_sprites@2x.scss',
-        engine: 'phantomjs',
-        padding: 2
-      }
     },
 
     // Allow async task running
@@ -173,6 +175,24 @@ module.exports = function(grunt) {
         logConcurrentOutput: true
       },
       compile: ['assemble','coffee', 'sass']
+    },
+
+    // Take a screenshot
+    autoshot: {
+      all: {
+        options: {
+          // necessary config
+          path: 'www/',
+          filename: 'screenshot.png',
+          type: 'png',
+          // optional config, must set either remote or local
+          local: {
+            path: 'www/',
+            port: 8080,
+          },
+          viewport: ['1024x768','640x1136','768x1024'] // Desktop, iPhone, iPad
+        },
+      },
     },
 
     // Deploy to a zip file
@@ -188,14 +208,13 @@ module.exports = function(grunt) {
     }
   });
 
-
   // Register tasks
 
   grunt.registerTask('build', function (target) {
     if (target === 'dev') {
       // Dev build
     } else { // Prod build
-      return grunt.task.run(['clean','copy','concurrent:compile']);
+      return grunt.task.run(['clean','copy','concurrent:compile','autoshot']);
     }
   });
 
@@ -220,13 +239,6 @@ module.exports = function(grunt) {
       'open',
       'watch'
     ]);
-  });
-
-  grunt.registerTask('screenshot', function () {
-    page.open('http://github.com/', function () {
-        page.render('github.png');
-        phantom.exit();
-    });
   });
 
   // Default tasks to be run.
